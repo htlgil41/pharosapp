f0f8e8847bc78d82617d13e250f9da7751687447
AARON ISRAEL <htlgil41@gmail.com>
2026-03-04 21:45:07 -0400

    Se agrego el paquete de jose para los jwt y se genero un path con los script necesarios para ejecutar la aplicacion

---
+++ scripts/jose_token.ts
@@ -0,0 +1,64 @@
+import fs from 'fs';
+import * as jose from 'jose';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const PATH_KEYS = path.resolve(
+    dirname(fileURLToPath(import.meta.url)),
+    '../confs/keysForTokens/'
+);
+
+const {
+    privateKey: priva_acces_token,
+    publicKey: public_access_token
+} = await jose.generateKeyPair(
+    'RS256',
+    { extractable: true, modulusLength: 2048 }
+);
+
+const {
+    privateKey: priva_refresh_token,
+    publicKey: public_refresh_token
+} = await jose.generateKeyPair(
+    'RS256',
+    { extractable: true, modulusLength: 2048 }
+);
+
+const pem_private_access_token = await jose.exportPKCS8(priva_acces_token);
+const pem_public_access_token = await jose.exportSPKI(public_access_token);
+
+const pem_private_refresh_token = await jose.exportPKCS8(priva_refresh_token);
+const pem_public_refresh_token = await jose.exportSPKI(public_refresh_token);
+
+
+const pemWritePrivateAccessToken = fs.createWriteStream(
+    `${PATH_KEYS}private_access_token.key`,
+    { encoding:'utf-8' }
+);
+
+const pemWritePublicAccessToken = fs.createWriteStream(
+    `${PATH_KEYS}public_access_token.pem`,
+    { encoding:'utf-8' }
+);
+
+const pemWritePrivateRefreshToken = fs.createWriteStream(
+    `${PATH_KEYS}private_refresh_token.key`,
+    { encoding:'utf-8' }
+);
+
+const pemWritePublicRefreshToken = fs.createWriteStream(
+    `${PATH_KEYS}public_refresh_token.pem`,
+    { encoding:'utf-8' }
+);
+
+pemWritePrivateAccessToken.write(pem_private_access_token);
+pemWritePublicAccessToken.write(pem_public_access_token);
+
+pemWritePrivateRefreshToken.write(pem_private_refresh_token);
+pemWritePublicRefreshToken.write(pem_public_refresh_token)
+
+pemWritePrivateAccessToken.close();
+pemWritePublicAccessToken.close();
+
+pemWritePrivateRefreshToken.close();
+pemWritePublicRefreshToken.close();
