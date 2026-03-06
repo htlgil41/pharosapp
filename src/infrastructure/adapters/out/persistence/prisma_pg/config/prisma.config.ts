bb07d6d5a9f07b12fded478225fcf562b9cf04db
AARON ISRAEL <htlgil41@gmail.com>
2026-03-05 20:06:10 -0400

    Configuracion del schema de prisma y la configuracion

--- src/infrastructure/adapters/out/persistence/prisma_pg/config/prisma.config.ts
+++ src/infrastructure/adapters/out/persistence/prisma_pg/config/prisma.config.ts
@@ -2,18 +2,19 @@ import "dotenv/config";
 import path, { dirname } from 'path';
 import { defineConfig } from "prisma/config";
 import { fileURLToPath } from "url";
+import { LoadEnvWithExceptions } from "../../../../../shareds/loadEnvWithExceptions.ts";
 
 const PATHGENERATEMODEL = path.resolve(
   dirname(fileURLToPath(import.meta.url)),
-  '../models/'
+  '../models'
 );
 
 export default defineConfig({
   schema: "schema.prisma",
   migrations: {
-    path: PATHGENERATEMODEL,
+    path: `${PATHGENERATEMODEL}`,
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: LoadEnvWithExceptions('DATABASE_PHAROSPP'),
   },
 });
