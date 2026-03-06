73a440ecbb04de6d2d4c52eb668264bd216ea1ec
AARON ISRAEL <htlgil41@gmail.com>
2026-03-05 20:10:54 -0400

    Renombre para el path de schema de prisma y se saco la configuracion de prisma a la raiz

--- src/infrastructure/adapters/out/persistence/prisma_pg/prisma.config.ts
+++ src/infrastructure/adapters/out/persistence/prisma_pg/prisma.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import path, { dirname } from 'path';
 import { defineConfig } from "prisma/config";
 import { fileURLToPath } from "url";
-import { LoadEnvWithExceptions } from "../../../../../shareds/loadEnvWithExceptions.ts";
+import { LoadEnvWithExceptions } from "../../../../shareds/loadEnvWithExceptions.ts";
 
 const PATHGENERATEMODEL = path.resolve(
   dirname(fileURLToPath(import.meta.url)),
