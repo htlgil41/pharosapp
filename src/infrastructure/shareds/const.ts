f87556b15957044a904be0c40b080d78d7c49bb1
AARON ISRAEL GIL NAVARRO <agil@maraplus.com>
2026-03-06 16:14:09 -0400

    Se agregaron las constantes de las keys de los jose token

---
+++ src/infrastructure/shareds/const.ts
@@ -0,0 +1,37 @@
+import * as jose from 'jose';
+import path, { dirname } from 'path';
+import fs from 'fs'; 
+import { fileURLToPath } from 'url';
+
+export const PATH = path.resolve(dirname(fileURLToPath(import.meta.url)));
+
+const loadKeyAccessToken = fs.readFileSync(
+    path.resolve(PATH, '../../../confs/keysForTokens/private_access_token.key'), { encoding: 'utf-8' });
+const loadPemAccessToken = fs.readFileSync(
+    path.resolve(PATH, '../../../confs/keysForTokens/public_access_token.pem'), { encoding: 'utf-8' });
+
+const loadKeyRefreshToken = fs.readFileSync(
+    path.resolve(PATH, '../../../confs/keysForTokens/private_refresh_token.key'), { encoding: 'utf-8' });
+const loadPemRefreshToken = fs.readFileSync(
+    path.resolve(PATH, '../../../confs/keysForTokens/public_refresh_token.pem'), { encoding: 'utf-8' });
+
+export const KEYACCESSTOKEN = await jose.importPKCS8(
+    loadKeyAccessToken,
+    'RS256',
+    { extractable: true }
+);
+export const PEMACCESSTOKEN = await jose.importSPKI(
+    loadPemAccessToken,
+    'RS256',
+    { extractable: true }
+);
+export const PEMAREFRESHTOKEN = await jose.importPKCS8(
+    loadKeyRefreshToken,
+    'RS256',
+    { extractable: true }
+);
+export const PEMREFRESHTOKEN = await jose.importSPKI(
+    loadPemRefreshToken,
+    'RS256',
+    { extractable: true }
+);
