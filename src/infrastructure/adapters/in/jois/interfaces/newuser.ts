49b60d6c4b4e60982834857d042692207a1ce5b6
AARON ISRAEL <htlgil41@gmail.com>
2026-03-07 18:04:27 -0400

    Mejor interfaz para la validacio del object de joi para login

--- src/infrastructure/adapters/in/jois/interfaces/newuser.ts
+++ src/infrastructure/adapters/in/jois/interfaces/newuser.ts
@@ -1,3 +1,12 @@
+export interface AuthLoginIUnterface {
+    username: string;
+    password: string;
+    farmacia_auth: {
+        id_farmacia: number;
+        farmacia: string;
+    }
+}
+
 export interface NewUserInterface {
     id_role: number | undefined;
     name_user: string;
