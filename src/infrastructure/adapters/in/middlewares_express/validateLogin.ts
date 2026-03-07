5fae3ac6575cac3a70f15c0cee2dc2d3942b96e8
AARON ISRAEL <htlgil41@gmail.com>
2026-03-07 13:16:50 -0400

    construyecndo las rutas de auth

--- src/infrastructure/adapters/in/middlewares_express/validateLogin.ts
+++ src/infrastructure/adapters/in/middlewares_express/validateLogin.ts
@@ -1,17 +1,20 @@
 import type { Request, Response, NextFunction } from 'express';
 import { AutLobinValidateJoi } from '../jois/objetcs.ts';
 
-export const ValidateLoginMiddleware = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) =>{
-    try {
-        
-       await AutLobinValidateJoi.validateAsync(req.body)
-       return next();
-    } catch (error) {
-        
-        throw new Error('Erro validate');
+export class ValidateAuthRouteMiddleware {
+
+    async validateLoginMiddleware (
+        req: Request,
+        _: Response,
+        next: NextFunction
+    ) {
+        try {
+            
+            await AutLobinValidateJoi.validateAsync(req.body)
+            return next();
+        } catch (error) {
+            
+            throw new Error('Erro validate');
+        }
     }
 }
---
+++ src/infrastructure/app/express/routes/auth.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { AuthRoute } from '../../../adapters/in/routes_express/authRoute.ts';
+import { ValidateAuthRouteMiddleware } from '../../../adapters/in/middlewares_express/validateLogin.ts';
+
+const authRouteIn = new AuthRoute();
+const middlewareValidate = new ValidateAuthRouteMiddleware();
+
+export const AuthRouter = Router()
+    .post('login',
+        middlewareValidate.validateLoginMiddleware,
+        authRouteIn.login
+    )
+    .post('usuario', authRouteIn.newUsuario);
