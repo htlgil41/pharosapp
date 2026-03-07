9b8294980bb497e6b44914c8923aa23b19134589
AARON ISRAEL <htlgil41@gmail.com>
2026-03-07 12:17:37 -0400

    Renombre del hashservide y se creo el tokejose service

---
+++ src/applicactions/services/tokenjose.ts
@@ -0,0 +1,57 @@
+import type { TokenJWTJOSEPort } from "../ports/token.ts";
+
+export class TokenJoseService {
+
+    constructor(
+        private jose: TokenJWTJOSEPort
+    ){}
+
+    async firmTokenAccess(data: string, expirateMinute: number): Promise<string> {
+        try {
+            
+            const tokenAccess = await this.jose.generateAccessToken(
+                data,
+                expirateMinute
+            );
+            const crypteToken = await this.jose.criptedToken(
+                tokenAccess,
+                expirateMinute
+            );
+            return crypteToken;
+        } catch (error) {
+            
+            throw new Error("Mal token generate en crypted");
+        }
+    }
+
+    async firmTokenRefresh(data: string, expirateMinute: number): Promise<string> {
+        try {
+            
+            const tokenAccess = await this.jose.generateRefresToken(
+                data,
+                expirateMinute
+            );
+            const crypteToken = await this.jose.criptedToken(
+                tokenAccess,
+                expirateMinute
+            );
+            return crypteToken;
+        } catch (error) {
+            
+            throw new Error("Mal token generate en crypted");
+        }
+    }
+
+    async validateAccessToken(
+        jwt: string
+    ): Promise<boolean> {
+        try {
+           
+            const descripted = await this.jose.validateAccessToken(jwt);
+            return true;
+        } catch (error) {
+            
+            throw new Error("Error al validar");
+        }
+    }
+}
