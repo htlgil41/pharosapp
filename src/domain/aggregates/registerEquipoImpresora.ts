6a375218084c1d16187be6fac34f6ba6d89d6d5a
AARON ISRAEL GIL NAVARRO <agil@maraplus.com>
2026-03-10 12:51:05 -0400

    Agregados de inventario para los equipos y inventario

--- src/domain/aggregates/registerEquipoImpresora.ts
+++ src/domain/aggregates/registerEquipoImpresora.ts
@@ -1,29 +1,29 @@
 import type { FarmaciaEntity } from "../entities/farmacia.ts";
+import type { ImpresoraEntity } from "../entities/impresora.ts";
 import { InventarioGeneralEntity } from "../entities/inventarioGeneral.ts";
-import type { PcEntity } from "../entities/pc.ts";
 
-export class RegisterEquipoPcFarmaciaAggregate {
+export class RegisterEquipoImpresoraFarmaciaAggregate {
 
     private inventario: InventarioGeneralEntity;
 
     constructor(
         private farmacia: FarmaciaEntity,
-        private pc: PcEntity,
+        private impresora: ImpresoraEntity,
         private cantidadSameEquipo: number,
     ) {
 
         const farmarmaciaPrimitive = farmacia.toValue();
-        const pcPrimitive = pc.toValue();
+        const impresoraPrimitive = impresora.toValue();
         this.inventario = InventarioGeneralEntity.build({
             id: 1,
             id_farmacia: farmarmaciaPrimitive.id,
             name_farmacia: farmarmaciaPrimitive.name_farmcia,
-            hardware: `PC con ${pcPrimitive.ram} de RAM y ${pcPrimitive.rom_size} de memoria con disco ${pcPrimitive.disk}`,
-            nota: `Nota de ingreso para ${farmarmaciaPrimitive.name_farmcia} - Eqiupo de escritorio o laptop`,
+            hardware: `Impresora ${impresoraPrimitive.marca} modelo ${impresoraPrimitive.modelo_print}`,
+            nota: `Nota de ingreso para ${farmarmaciaPrimitive.name_farmcia} - Impresora ${impresoraPrimitive.marca}/${impresoraPrimitive.modelo_print}`,
             cantidad: cantidadSameEquipo <= 0 ? 1 : cantidadSameEquipo,
         });
     }
 
     getInventario = () => this.inventario;
-    getEquipo = () => this.pc;
+    getEquipo = () => this.impresora;
 }
--- src/domain/aggregates/registerEquipoPuntoVenta.ts
+++ src/domain/aggregates/registerEquipoPuntoVenta.ts
@@ -1,29 +1,30 @@
 import type { FarmaciaEntity } from "../entities/farmacia.ts";
+import type { ImpresoraEntity } from "../entities/impresora.ts";
 import { InventarioGeneralEntity } from "../entities/inventarioGeneral.ts";
-import type { PcEntity } from "../entities/pc.ts";
+import type { PuntoVentaEntity } from "../entities/puntoVenta.ts";
 
-export class RegisterEquipoPcFarmaciaAggregate {
+export class RegisterPuntoVentaFarmaciaAggregate {
 
     private inventario: InventarioGeneralEntity;
 
     constructor(
         private farmacia: FarmaciaEntity,
-        private pc: PcEntity,
+        private punto: PuntoVentaEntity,
         private cantidadSameEquipo: number,
     ) {
 
         const farmarmaciaPrimitive = farmacia.toValue();
-        const pcPrimitive = pc.toValue();
+        const puntoPrimitive = punto.toValue();
         this.inventario = InventarioGeneralEntity.build({
             id: 1,
             id_farmacia: farmarmaciaPrimitive.id,
             name_farmacia: farmarmaciaPrimitive.name_farmcia,
-            hardware: `PC con ${pcPrimitive.ram} de RAM y ${pcPrimitive.rom_size} de memoria con disco ${pcPrimitive.disk}`,
-            nota: `Nota de ingreso para ${farmarmaciaPrimitive.name_farmcia} - Eqiupo de escritorio o laptop`,
+            hardware: `${puntoPrimitive.banco} punto de venta modelo ${puntoPrimitive.modelo} serial ${puntoPrimitive.serial_code}`,
+            nota: `Nota de ingreso para ${farmarmaciaPrimitive.name_farmcia} - Punto de venta modelo ${puntoPrimitive.serial_code}/${puntoPrimitive.modelo} de ${puntoPrimitive.banco}`,
             cantidad: cantidadSameEquipo <= 0 ? 1 : cantidadSameEquipo,
         });
     }
 
     getInventario = () => this.inventario;
-    getEquipo = () => this.pc;
+    getEquipo = () => this.punto;
 }
