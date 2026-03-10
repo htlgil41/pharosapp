678ad5614a4c3d4ff3b0d483184502e2d4e57938
AARON ISRAEL <htlgil41@gmail.com>
2026-03-09 22:12:57 -0400

    Caso para agregar farmacia

---
+++ src/applicactions/usesCases/newCajaFarmacia.ts
@@ -0,0 +1,55 @@
+import { CajaFarmaciaEntity } from "../../domain/entities/cajaFarmacia.ts";
+import type { FarmaciaRepository } from "../../domain/repositories/farmacia.ts";
+import { AuthorizationExceptionUseCase } from "../exceptions/authorization.ts";
+import { DataNotFoundExceptionUseCase } from "../exceptions/dataNotFound.ts";
+import type { DataAccessToken } from "../ports/token.ts";
+import { ServiceAuthorization } from "../services/authorization.ts";
+
+interface NewCajaDTO {
+    idFarmacia: number;
+    nmCaja: number;
+    area: string
+}
+
+interface CajaDTO {
+    farmacia: string;
+    nmCaja: number
+}
+
+export class NewCajaFarmaciaUseCase {
+    constructor(
+        private repo: FarmaciaRepository
+    ){}
+
+    async execute(
+        dataUsuario: DataAccessToken,
+        dto: NewCajaDTO,
+    ): Promise<CajaDTO> {
+        
+        if (!ServiceAuthorization.accessMulti(['coordinador', 'soportista'], dataUsuario.role))
+            throw new AuthorizationExceptionUseCase();
+
+        const farmacia = await this.repo.getFarmaciaById(dto.idFarmacia);
+        if (!farmacia) throw new DataNotFoundExceptionUseCase(
+            'No se encontro la farmacia',
+            'La farmacia no existe por lo la caja no puede ser creada verifica la informacion e intenta nuevamente',
+            ''
+        );
+
+        const farmaciaPrimitive = farmacia.toValue();
+        const createCaja = await this.repo.creteCajaFarmacia(
+            farmacia,
+            CajaFarmaciaEntity.build({
+                id: 1,
+                area: dto.area,
+                id_farmacia: farmaciaPrimitive.id,
+                name_farmacia: farmaciaPrimitive.name_farmcia,
+                nm_caja: dto.nmCaja
+            }),
+        );
+        return {
+            farmacia: farmaciaPrimitive.name_farmcia,
+            nmCaja: dto.nmCaja,
+        };
+    }
+}
