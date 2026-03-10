16d8b0df90228d16491df2e7d5e2efacbc95360e
AARON ISRAEL GIL NAVARRO <agil@maraplus.com>
2026-03-10 11:08:58 -0400

    Se creo un agregado para las pc

---
+++ src/domain/aggregates/registerEquipoPc.ts
@@ -0,0 +1,29 @@
+import type { FarmaciaEntity } from "../entities/farmacia.ts";
+import { InventarioGeneralEntity } from "../entities/inventarioGeneral.ts";
+import type { PcEntity } from "../entities/pc.ts";
+
+export class RegisterEquipoPcFarmaciaAggregate {
+
+    private inventario: InventarioGeneralEntity;
+
+    constructor(
+        private farmacia: FarmaciaEntity,
+        private pc: PcEntity,
+        private cantidadSameEquipo: number,
+    ) {
+
+        const farmarmaciaPrimitive = farmacia.toValue();
+        const pcPrimitive = pc.toValue();
+        this.inventario = InventarioGeneralEntity.build({
+            id: 1,
+            id_farmacia: farmarmaciaPrimitive.id,
+            name_farmacia: farmarmaciaPrimitive.name_farmcia,
+            hardware: `PC con ${pcPrimitive.ram} de RAM y ${pcPrimitive.rom_size} de memoria con disco ${pcPrimitive.disk}`,
+            nota: `Nota de ingreso para ${farmarmaciaPrimitive.name_farmcia} de un eqiupo de escritorio o laptop`,
+            cantidad: cantidadSameEquipo <= 0 ? 1 : cantidadSameEquipo,
+        });
+    }
+
+    getInventario = () => this.inventario;
+    getEquipo = () => this.pc;
+}
